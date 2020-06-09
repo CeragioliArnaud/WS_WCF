cbc128ea020e11f2eddf5b8dfd562b077252d7bb
Quentin Guisisiano <querzanvarak@gmail.com>
2020-06-09 11:19:24 +0200

    Contract

---
+++ eXia_A4L_WS_SECUWCF_SERVWCF_CONTRACT/eXia_A4L_WS_SECUWCF_SERVWCF_CONTRACT/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace eXia_A4L_WS_SECUWCF_SERVWCF_CONTRACT
+{
+    [ServiceContract]
+
+    public interface iSVR
+    {
+        [System.ServiceModel.OperationContract]
+        STC_MSG m_service(STC_MSG msg);
+    }
+    [DataContract]
+    public struct STC_MSG
+    {
+        [System.Runtime.Serialization.DataMember]
+        public bool op_statut;
+        [System.Runtime.Serialization.DataMember]
+        public string op_name;
+        [System.Runtime.Serialization.DataMember]
+        public string op_info;
+        [System.Runtime.Serialization.DataMember]
+        public string app_name;
+        [System.Runtime.Serialization.DataMember]
+        public string app_version;
+        [System.Runtime.Serialization.DataMember]
+        public string app_token;
+        [System.Runtime.Serialization.DataMember]
+        public string user_login;
+        [System.Runtime.Serialization.DataMember]
+        public string user_psw;
+        [System.Runtime.Serialization.DataMember]
+        public string user_token;
+        [System.Runtime.Serialization.DataMember]
+        public object[] data;
+
