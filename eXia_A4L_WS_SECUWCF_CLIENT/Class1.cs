0508541e50856e7b173dcd2be039f82ca5648a9c
Quentin Guisisiano <querzanvarak@gmail.com>
2020-06-09 11:56:09 +0200

    c le bordel

---
+++ eXia_A4L_WS_SECUWCF_CLIENT/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using eXia_A4L_WS_SECUWCF_CLIENT.proxy;
+
+namespace eXia_A4L_WS_SECUWCF_CLIENT.M1_CUT
+{
+    public class CUT_AUTH
+    {
+        private STC_MSG msg;
+        private eXia_A4L_WS_SECUWCF_CLIENT.M2_CUC.CUC cuc;
+
+        public CUT_AUTH()
+        {
+            this.msg = new STC_MSG();
+            this.cuc = new M2_CUC.CUC();
+        }
+
+        public STC_MSG m_auhentifier(STC_MSG msg)
+        {
+            this.msg = msg;
+            this.msg.op_name = "Call from the CUC";
+            this.msg = this.cuc.m_sendSVER(this.msg);
+
+            return this.msg;
+        }
+    }
+}
---
+++ eXia_A4L_WS_SECUWCF_CLIENT/Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using eXia_A4L_WS_SECUWCF_CLIENT.proxy;
+
+namespace eXia_A4L_WS_SECUWCF_CLIENT.M2_CUC
+{
+    public class CUC
+    {
+
+        private proxy.STC_MSG msg;
+        private proxy.E2*******************; prox;
+        
+        public CUC()
+        {
+            this.msg = new STC_MSG();
+            this.prox = new E2*******************.Client();
+
+        }
+
+        public STC_MSG m_sendSVER(STC_MSG msg)
+        {
+            this.msg = msg;
+            this.msg.app_name = "Appli1";
+            this.msg.app_token = "12345";
+            this.msg.app_version = "10";
+            this.msg.op_info = "Demande de service de l'application 1 de version 1.0";
+
+            this.msg = this.prox.m_server(this.msg);
+
+            return this.msg;
+        }
+    }
+}
